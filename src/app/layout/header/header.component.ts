bd4a3ae97294b2124a289b814f909532333263b4
Tiiliya <Tiiliya@126.com>
2018-04-25 18:53:54 +0800

    header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -9,6 +9,20 @@ import { EventEmitter } from '@angular/core';
 export class HeaderComponent implements OnInit {
   @Input() flag:boolean;
   @Output() triggerSlide: EventEmitter<any> = new EventEmitter()
+
+  messages: Array<string> = ['5 mins','2 hours','Today','Yesterdy']
+  notice: Array<any> = [
+    { color: '#00c0ef',icon:'anticon anticon-check-circle'},
+    { color: '#f39c12',icon:'anticon anticon-area-chart'},
+    { color: '#dd4b39',icon:'anticon anticon-cloud'},
+    { color: '#00a65a',icon:'anticon anticon-shopping-cart'},
+  ]
+  tasks: Array<any> = [
+    { color: '#00c0ef',width:'20%'},
+    { color: '#f39c12',width:'40%'},
+    { color: '#dd4b39',width:'60%'},
+    { color: '#00a65a',width:'80%'}
+  ]
   constructor() { }
 
   ngOnInit() {
--- src/app/layout/header/header.module.ts
+++ src/app/layout/header/header.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 
 import { HeaderComponent } from './header.component';
+import { NgZorroAntdModule } from "ng-zorro-antd";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
     imports: [
-       
+        CommonModule,
+        NgZorroAntdModule
     ],
     declarations: [
         HeaderComponent
